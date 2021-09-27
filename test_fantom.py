d0583927e890bc989e1ceb01fc92323fb9dafccb
Lucas Marcel <lucas.marcel@epitech.eu>
2021-09-28 01:33:33 +0200

    add new functions / update_game_state_suspects

--- test_fantom.py
+++ test_fantom.py
@@ -31,55 +31,6 @@ stream_handler.setLevel(logging.WARNING)
 fantom_logger.addHandler(stream_handler)
 
 
-class Player():
-
-    def __init__(self):
-
-        self.end = False
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-    def connect(self):
-        self.socket.connect((host, port))
-
-    def reset(self):
-        self.socket.close()
-
-    def answer(self, question):
-        # work
-        data = question["data"]
-        game_state = question["game state"]
-        print_game_info(data, game_state)
-        response_index = random.randint(0, len(data)-1)
-        # log
-        fantom_logger.debug("|\n|")
-        fantom_logger.debug("fantom answers")
-        fantom_logger.debug(f"question type ----- {question['question type']}")
-        fantom_logger.debug(f"data -------------- {data}")
-        fantom_logger.debug(f"response index ---- {response_index}")
-        fantom_logger.debug(f"response ---------- {data[response_index]}")
-        return response_index
-
-    def handle_json(self, data):
-        data = json.loads(data)
-        response = self.answer(data)
-        # send back to server
-        bytes_data = json.dumps(response).encode("utf-8")
-        protocol.send_json(self.socket, bytes_data)
-
-    def run(self):
-
-        self.connect()
-
-        while self.end is not True:
-            received_message = protocol.receive_json(self.socket)
-            if received_message:
-                self.handle_json(received_message)
-            else:
-                print("no message, finished learning")
-                self.end = True
-
-
 def print_game_info(data, game_state):
     print("DATA -----\n")
     print(data)
@@ -87,7 +38,12 @@ def print_game_info(data, game_state):
     print("GAME State -----\n")
     print(game_state)
     print("-----\n")
+    print("Score at current Game_State -----\n")
     print(calculate_score(game_state))
+    print("-----\n")
+    print("Update Game_State Suspects -----\n")
+    print(update_game_state_suspects(game_state))
+    print("-----\n")
 
 
 def get_current_score_from_game_state(game_state):
@@ -147,6 +103,12 @@ def is_character_alone(game_state, color):
     return True if nb == 1 else False
 
 
+def is_character_in_shadow(game_state, color):
+    character = get_character_by_color(game_state, color)
+
+    return character["position"] == get_shadow_from_game_state(game_state)
+
+
 def calculate_score(game_state):
     score = get_current_score_from_game_state(game_state)
     fantom = get_character_by_color(game_state, get_fantom_from_game_state(game_state))
@@ -154,24 +116,104 @@ def calculate_score(game_state):
     fantom_is_alone = is_character_alone(game_state, fantom["color"])
 
     if fantom["position"] == get_shadow_from_game_state(game_state) or fantom_is_alone:
-        # Fantom alone or in the shadow / The Fantom shouts (score +1)
+        # Fantom is alone or in the shadow / The Fantom shouts (score +1)
         score += 1
         for suspect in suspects:
             if is_character_alone(game_state, suspect["color"]):
                 # Suspect alone (score +1)
                 score += 1
                 continue
-            if suspect["position"] == get_shadow_from_game_state(game_state):
+            if is_character_in_shadow(game_state, suspect["color"]):
                 # Suspect in the shadow (score +1)
                 score += 1
     else:
         # Fantom in a group
         for suspect in suspects:
-            if not is_character_alone(game_state, suspect["color"]) and not suspect["position"] == get_shadow_from_game_state(game_state):
-                # Suspect not alone and not in the shadow / in a group (score +1)
+            if not is_character_alone(game_state, suspect["color"]) \
+                    and not is_character_in_shadow(game_state, suspect["color"]):
+                # Suspect is not alone and not in the shadow / in a group (score +1)
                 score += 1
 
     return score
+
+
+def update_game_state_suspects(game_state):
+    fantom = get_character_by_color(game_state, get_fantom_from_game_state(game_state))
+    suspects = get_suspects_from_game_state(game_state)
+    fantom_is_alone = is_character_alone(game_state, fantom["color"])
+
+    if fantom["position"] == get_shadow_from_game_state(game_state) or fantom_is_alone:
+        for suspect in suspects:
+            if not is_character_alone(game_state, suspect["color"]) \
+                    and not is_character_in_shadow(game_state, suspect["color"]):
+                for character in get_characters_from_game_state(game_state):
+                    if suspect["color"] == character["color"]:
+                        character["suspect"] = False
+                        break
+    else:
+        for suspect in suspects:
+            if is_character_alone(game_state, suspect["color"]):
+                for character in get_characters_from_game_state(game_state):
+                    if suspect["color"] == character["color"]:
+                        character["suspect"] = False
+                        break
+                continue
+            if is_character_in_shadow(game_state, suspect["color"]):
+                for character in get_characters_from_game_state(game_state):
+                    if suspect["color"] == character["color"]:
+                        character["suspect"] = False
+                        break
+
+    return game_state
+
+
+class Player():
+
+    def __init__(self):
+
+        self.end = False
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    def connect(self):
+        self.socket.connect((host, port))
+
+    def reset(self):
+        self.socket.close()
+
+    def answer(self, question):
+        # work
+        data = question["data"]
+        game_state = question["game state"]
+        print_game_info(data, game_state)
+        response_index = random.randint(0, len(data)-1)
+        # log
+        fantom_logger.debug("|\n|")
+        fantom_logger.debug("fantom answers")
+        fantom_logger.debug(f"question type ----- {question['question type']}")
+        fantom_logger.debug(f"data -------------- {data}")
+        fantom_logger.debug(f"response index ---- {response_index}")
+        fantom_logger.debug(f"response ---------- {data[response_index]}")
+        return response_index
+
+    def handle_json(self, data):
+        data = json.loads(data)
+        response = self.answer(data)
+        # send back to server
+        bytes_data = json.dumps(response).encode("utf-8")
+        protocol.send_json(self.socket, bytes_data)
+
+    def run(self):
+
+        self.connect()
+
+        while self.end is not True:
+            received_message = protocol.receive_json(self.socket)
+            if received_message:
+                self.handle_json(received_message)
+            else:
+                print("no message, finished learning")
+                self.end = True
 
 
 p = Player()
